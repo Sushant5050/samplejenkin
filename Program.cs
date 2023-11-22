6760c497bf72e23ea323318763d604ad0ce930e4
Sushant5050 <148970856+Sushant5050@users.noreply.github.com>
2023-11-22 14:11:22 +0530

    hhh

--- Program.cs
+++ Program.cs
@@ -12,14 +12,14 @@ namespace samplejenkin
         {
 
             int a = 10, b = 20, c;
-            c = a + b
+            c = a + b;
             Console.WriteLine("Addition=" + c);
 
         }
         static void Main()
         {
             Program p = new Program();
-            p.Addition();
+            p.Addition(
             Console.ReadLine();
 
         }
