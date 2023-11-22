292cc9d624bc1898bfb411a0955a79d15b0185c2
Sushant5050 <148970856+Sushant5050@users.noreply.github.com>
2023-11-22 13:13:47 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace samplejenkin
+{
+    internal class Program
+    {
+        public void Addition()
+        {
+
+            int a = 10, b = 20, c;
+            c = a + b;
+            Console.WriteLine("Addition=" + c);
+
+        }
+        static void Main()
+        {
+            Program p = new Program();
+            p.Addition();
+            Console.ReadLine();
+
+        }
+    }
+}
+
